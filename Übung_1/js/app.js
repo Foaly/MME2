f89692b5f2a38893ab6704856308148b4bfc06cd
Maximilian Wagenbach <s52211@beuth-hochschule.de>
2015-04-13 20:36:34 +0200

    Added the styling and the filter functionality. Also show the html5 controllers when in fullscreen.

--- Übung_1/js/app.js
+++ Übung_1/js/app.js
@@ -7,26 +7,31 @@ window.onload = function() {
     document.getElementById("stopButton").addEventListener("click", stopCallback);
     document.getElementById("fullscreenButton").addEventListener("click", fullscreenCallback);
     videoplayer.addEventListener("timeupdate", videoPlaybackCallback);
+    document.getElementById("filterinput").addEventListener("change", filterCallback);
     
     var progressBar = document.getElementById("progressBar");
     progressBar.max = videoplayer.duration;
     progressBar.addEventListener("input", progressBarCallback);
+    
+    // hide the controls if we leave fullscreen mode
+    document.addEventListener("mozfullscreenchange", function() { if (!document.mozFullScreenElement) { videoplayer.controls = false; } });
+    document.addEventListener("webkitExitFullscreen", function() { videoplayer.controls = false; }); // TODO: test this on chrome
 }
 
 var playpauseCallback = function() {
     if(videoplayer.paused) {
         videoplayer.play();
-        document.getElementById("playPauseButton").textContent = "\u258D \u258D Pause";
+        document.getElementById("playPauseButton").textContent = "\u258D \u258D";
     }
     else {
         videoplayer.pause();
-        document.getElementById("playPauseButton").textContent = "\u25B6 Play";
+        document.getElementById("playPauseButton").textContent = "\u25B6";
     }
 }
 
 var stopCallback = function() {
     videoplayer.pause();
-    document.getElementById("playPauseButton").textContent = "\u25B6 Play";
+    document.getElementById("playPauseButton").textContent = "\u25B6";
     videoplayer.currentTime = 0;
 }
 
@@ -40,12 +45,15 @@ var fullscreenCallback = function() {
     } else if (videoplayer.webkitRequestFullscreen) {
       videoplayer.webkitRequestFullscreen();
     }
+    
+    // show the controls in fullscreen mode
+    videoplayer.controls = true;
 }
 
     
 var videoPlaybackCallback = function() {
     // convert seconds into string with the format HH:MM:SS:MSMS
-    var currentTime = document.getElementById("videoplayer").currentTime.toPrecision(2);
+    var currentTime = videoplayer.currentTime.toPrecision(2);
     var numberOfSeconds = parseInt(currentTime, 10);
     var hours   = Math.floor(numberOfSeconds / 3600);
     var minutes = Math.floor((numberOfSeconds - (hours * 3600)) / 60);
@@ -66,4 +74,16 @@ var videoPlaybackCallback = function() {
 
 var progressBarCallback = function() {
     document.getElementById("videoplayer").currentTime = document.getElementById("progressBar").value;
+}
+
+var filterCallback = function() {
+    var filters = "";
+    if(document.getElementById("sepiaBox").checked)
+        filters += "sepia(100%) ";    
+    if(document.getElementById("grayscaleBox").checked)
+        filters += "grayscale(100%) ";
+    if(document.getElementById("blurBox").checked)
+        filters += "blur(3px) ";
+    
+    videoplayer.style = "filter: " + filters + ";";
 }
